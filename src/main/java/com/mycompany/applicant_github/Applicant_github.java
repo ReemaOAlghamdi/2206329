3630c5e4b36f393518116240b27d8463cd664ecc
reema <reema@REMO>
2025-04-14 01:50:24 +0300

    Applicant class

---
+++ src/main/java/com/mycompany/applicant_github/Applicant_github.java
@@ -0,0 +1,8 @@
+class Applicant {
+    private final Scanner scanner;
+    private final FileHandler fileHandler;
+
+    public Applicant(Scanner scanner, FileHandler fileHandler) {
+        this.scanner = scanner;
+        this.fileHandler = fileHandler;
+    }
